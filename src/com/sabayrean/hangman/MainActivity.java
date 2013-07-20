93e034db4467e61f26eff4d55306c57798245f88
kakada <kakada@kakada-ThinkPad-T410.(none)>
2013-07-20 16:02:57 +0700

    initialize view

---
+++ src/com/sabayrean/hangman/MainActivity.java
@@ -0,0 +1,21 @@
+package com.sabayrean.hangman;
+
+import com.sabayrean.hangmain.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+				InputMethodManager.HIDE_IMPLICIT_ONLY);
+	}
+}
